0a744626b8c5fa34cdff3d487a4d2f3392dfd84c
corka149 <corka149@mailbox.org>
2018-04-29 16:09:09 +0200

    Made it more idiomatic

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-use std::str::Chars;
 use std::ops::Index;
 use std::io::{self, BufRead};
 
@@ -14,26 +13,15 @@ fn main() {
             // Accessing values is safe because len was checked.
             let offsets = splits.get(0).unwrap();
             let text = splits.get(1).unwrap();
-            let new_offsets = match_to_same_len(&text, offsets);
-            process_strings(text.chars(), new_offsets.chars());
+            println!("{}", cipher(text, offsets));
         }
     }
 }
 
-fn match_to_same_len<'a>(text: &'a str, offsets: &'a str) -> String {
-    let off_len = offsets.len();
-    let mut new_offset = String::from(offsets);
-
-    for _ in 0..(text.len() / off_len) {
-        new_offset.push_str(offsets);
-    }
-
-    new_offset.push_str(offsets.index(0..(text.len() % off_len)));
-    new_offset
-}
-
-fn process_strings(text: Chars, offsets: Chars) {
-    text.zip(offsets).map(|a| print!("{}", encode(a.0, a.1)));
+fn cipher<'a>(text: &'a str, offsets: &'a str) -> String {
+    text.chars()
+        .zip(offsets.chars().cycle()).map(|(t, o)| encode(t, o))
+        .collect()
 }
 
 fn encode(x: char, offset: char) -> char {
